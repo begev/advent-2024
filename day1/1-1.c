dd7329336163efbcfef6dcbf2886fdde454a5a91
begev <151647367+begev@users.noreply.github.com>
2024-12-18 01:19:33 +0200

    Add day1 tasks

---
+++ day1/1-1.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int comp(const void *a, const void *b) {
+
+	
+	int int_a, int_b;
+
+	int_a = *(int *)a;
+	int_b = *(int *)b;
+
+	return (int_a > int_b) - (int_a < int_b);
+	
+}
+
+
+int main(void) {
+	
+	int *array1, *array2;
+	
+	FILE *input;
+
+	char row[100];
+	char numchar1[6], numchar2[6];
+
+	int i, row_index;
+
+	int sum;
+	
+
+	/*Input file has 1000 rows*/
+	array1 = malloc(sizeof(int) * 1000);
+	array2 = malloc(sizeof(int) * 1000);
+
+	input = fopen("input.txt", "r");
+
+	row_index = 0;
+	while(fgets(row, 14, input)) {
+
+		/*Skip wrongly formatted lines*/
+		if (strlen(row) < 13) continue;
+		
+		for (i = 0; i < 5; i++) {
+			numchar1[i] = row[i];
+			numchar2[i] = row[8+i];
+
+		}
+
+		array1[row_index] = atoi(numchar1);
+		array2[row_index] = atoi(numchar2);
+		row_index++;
+
+	}
+
+	fclose(input);
+	
+	qsort(array1, 1000, sizeof(int), comp);
+	qsort(array2, 1000, sizeof(int), comp);
+
+	sum = 0;
+	for (i = 0; i < 1000; i++) {
+		sum += abs(array1[i]-array2[i]);
+	}
+	
+	printf("%d\n", sum);
+			 
+}
+	
